0f655f09a3fe2998fabbd33a31e6c5614ad82eab
Sharvil Kesarwani <sharkesa@gmail.com>
2019-12-21 21:50:38 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import cv2
 import editdistance
 import argparse
@@ -6,13 +7,20 @@ from Loading_Data import Load_Data, Batch
 from Neural_Network import Neural_Network
 from Preprocess import preprocess
 
+
+class directory:
+    '''directory path'''
+    fn_directory = os.path.dirname(os.path.realpath(__file__))
+
+
 class paths:
     '''Paths to data'''
-    fn_characters = '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/save/characters.txt'
-    fn_accuracy = '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/save/accuracy.txt'
-    fn_train = '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/data/'
-    fn_predict = '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/data/test.png'
-    fn_texts = '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/data/texts.txt'
+    fn_characters = directory.fn_directory + '/save/characters.txt'
+    fn_accuracy = directory.fn_directory + '/save/accuracy.txt'
+    fn_train = directory.fn_directory + '/data/'
+    fn_predict = directory.fn_directory + '/data/test.png'
+    fn_texts = directory.fn_directory + '/data/texts.txt'
+
 
 def train(network, loader):
     '''Train the Neural Network'''
@@ -28,7 +36,13 @@ def train(network, loader):
             batch_info = loader.batch_info()
             next_batch = loader.next()
             cost = network.batching(next_batch)
-            print('Batch', batch_info[0], '/', batch_info[1], 'trained. Current cost:', cost)
+            print(
+                'Batch',
+                batch_info[0],
+                '/',
+                batch_info[1],
+                'trained. Current cost:',
+                cost)
 
         character_error = test(network, loader)
         # output error rates and keep improving
@@ -36,13 +50,20 @@ def train(network, loader):
             best_error = character_error
             last_improve = 0
             network.save()
-            open(paths.fn_accuracy, 'w').write('Network\'s current error rate: %f%%' % (character_error * 100))
+            open(
+                paths.fn_accuracy, 'w').write(
+                'Network\'s current error rate: %f%%' %
+                (character_error * 100))
         else:
             last_improve += 1
 
         if last_improve >= stopping:
-            print('No improvement in the last', stopping, 'epochs. Training completed.')
+            print(
+                'No improvement in the last',
+                stopping,
+                'epochs. Training completed.')
             break
+
 
 def test(network, loader):
     '''Test the Neural Network'''
@@ -68,44 +89,84 @@ def test(network, loader):
     # output error rates
     error_rate = num_errors / num_errors_total
     accuracy = num_words / num_words_total
-    print('Character error rate: %f%% | Word accuracy: %f%%' % (error_rate * 100.0, accuracy * 100.0))
+    print('Character error rate: %f%% | Word accuracy: %f%%' %
+          (error_rate * 100.0, accuracy * 100.0))
     return error_rate
+
 
 def use(network, fn_image):
     '''recognise text in images provided by file path'''
-    image = preprocess(cv2.imread(fn_image, cv2.IMREAD_GRAYSCALE), Neural_Network.image_size)
+    image = preprocess(
+        cv2.imread(
+            fn_image,
+            cv2.IMREAD_GRAYSCALE),
+        Neural_Network.image_size)
     batch1 = Batch(None, [image])
     word = network.test_batch(batch=batch1)
     print('Translation:', word)
 
+
 def main():
     '''The actual program running'''
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train', help='Train the Neural Network', action='store_true')
-    parser.add_argument('--test', help='Test the Neural Network', action='store_true')
-    parser.add_argument('--use', help='Uses and saves the outputs of the Neural Networks onto CSV files.', action='store_true')
+    parser.add_argument(
+        '--train',
+        help='Train the Neural Network on the given dataset. For more information, read the README file.',
+        action='store_true')
+    parser.add_argument(
+        '--test',
+        help='Test the Neural Network on the given dataset. For more information, read the README file.',
+        action='store_true')
+    parser.add_argument(
+        '--use',
+        help='Uses and saves the outputs of the Neural Networks onto CSV files. For more information, read the README file.',
+        action='store_true')
 
     args = parser.parse_args()
+    # tells program to train the network
     if args.train:
-        loader = Load_Data(paths.fn_train, Neural_Network.batch_size, Neural_Network.image_size, Neural_Network.text_len)
+        loader = Load_Data(
+            paths.fn_train,
+            Neural_Network.batch_size,
+            Neural_Network.image_size,
+            Neural_Network.text_len)
         open(paths.fn_characters, 'w').write(str().join(loader.characters))
-        open(paths.fn_texts, 'w').write(str(' ').join(loader.training_words + loader.testing_words))
+        open(
+            paths.fn_texts,
+            'w').write(
+            str(' ').join(
+                loader.training_words +
+                loader.testing_words))
 
         network = Neural_Network(loader.characters)
         train(network, loader)
-
+    # tells program to test the network
     elif args.test:
-        loader = Load_Data(paths.fn_train, Neural_Network.batch_size, Neural_Network.image_size, Neural_Network.text_len)
+        loader = Load_Data(
+            paths.fn_train,
+            Neural_Network.batch_size,
+            Neural_Network.image_size,
+            Neural_Network.text_len)
         open(paths.fn_characters, 'w').write(str().join(loader.characters))
-        open(paths.fn_texts, 'w').write(str(' ').join(loader.training_words + loader.testing_words))
-        
+        open(
+            paths.fn_texts,
+            'w').write(
+            str(' ').join(
+                loader.training_words +
+                loader.testing_words))
+
         network = Neural_Network(loader.characters, restore=True)
         test(network, loader)
-
+    # tells program to use the network
     else:
         print(open(paths.fn_accuracy).read())
-        network = Neural_Network(open(paths.fn_characters).read(), saved=args.save, restore=True)
+        network = Neural_Network(
+            open(
+                paths.fn_characters).read(),
+            saved=args.use,
+            restore=True)
         use(network, paths.fn_predict)
+
 
 if __name__ == '__main__':
     main()
