77a0dab0ce8e691e1c0dfe6a982e5ce6f60e11e7
Sharvil Kesarwani <sharkesa@gmail.com>
2019-12-21 21:45:06 +1100

    Update Neural_Network.py

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,6 +1,11 @@
 import cv2
-import tensorflow as tf
 import os
+import tensorflow as tf
+
+
+class filepath:
+    fn_filepath = os.path.dirname(os.path.realpath(__file__))
+
 
 class Neural_Network:
 
@@ -17,7 +22,12 @@ class Neural_Network:
         self.current = 0
 
         # inputting the images in batches
-        self.batches = tf.placeholder(tf.float32, shape=(None, Neural_Network.image_size[0], Neural_Network.image_size[1]))
+        self.batches = tf.placeholder(
+            tf.float32,
+            shape=(
+                None,
+                Neural_Network.image_size[0],
+                Neural_Network.image_size[1]))
 
         # setting up the required neural networks
         self.Convolutional_Neural_Network()
@@ -29,7 +39,8 @@ class Neural_Network:
         self.rate = tf.placeholder(tf.float32, shape=[])
         self.updates = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(self.updates):
-            self.optimiser = tf.train.AdamOptimizer(self.rate).minimize(self.cost)
+            self.optimiser = tf.train.AdamOptimizer(
+                self.rate).minimize(self.cost)
 
         (self.sess, self.saver) = self.setup()
 
@@ -39,14 +50,17 @@ class Neural_Network:
         sess = tf.Session()
 
         saver = tf.train.Saver(max_to_keep=1)
-        save_dir = '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/save/'
+        save_dir = filepath.fn_filepath + '/save/'
         latest = tf.train.latest_checkpoint(save_dir)
 
         if self.restore and not latest:
             raise Exception('No network currently saved')
 
         if latest:
-            print('Network', latest, 'found. Proceeding to initiate with stored values.')
+            print(
+                'Network',
+                latest,
+                'found. Proceeding to initiate with stored values.')
             saver.restore(sess, latest)
         else:
             print('No network found. Proceeding to initiate with new values.')
@@ -68,9 +82,24 @@ class Neural_Network:
         # create layers
         data = conv4d_in
         for layer in range(number_layers):
-            weight = tf.Variable(tf.random_normal([weight_values[layer], weight_values[layer], feature_values[layer], feature_values[layer + 1]]))
-            conv = tf.nn.relu(tf.layers.batch_normalization(tf.nn.conv2d(data, weight, strides=(1, 1, 1, 1), padding='SAME')))
-            data = tf.nn.max_pool(conv, (1, pool_values[layer][0], pool_values[layer][1], 1), (1, stride_values[layer][0], stride_values[layer][1],1), padding='VALID')
+            weight = tf.Variable(tf.random_normal(
+                [weight_values[layer], weight_values[layer], feature_values[layer], feature_values[layer + 1]]))
+            conv = tf.nn.relu(
+                tf.layers.batch_normalization(
+                    tf.nn.conv2d(
+                        data, weight, strides=(
+                            1, 1, 1, 1), padding='SAME')))
+            data = tf.nn.max_pool(
+                conv,
+                (1,
+                 pool_values[layer][0],
+                 pool_values[layer][1],
+                 1),
+                (1,
+                 stride_values[layer][0],
+                 stride_values[layer][1],
+                 1),
+                padding='VALID')
 
         self.conv4d_out = data
 
@@ -86,12 +115,15 @@ class Neural_Network:
         stack = tf.nn.rnn_cell.MultiRNNCell(lstm_cells)
 
         # construct information flow in both directions
-        ((forward, backward), _) = tf.nn.bidirectional_dynamic_rnn(cell_fw=stack, cell_bw=stack, inputs=rec3d_in, dtype=rec3d_in.dtype)
+        ((forward, backward), _) = tf.nn.bidirectional_dynamic_rnn(
+            cell_fw=stack, cell_bw=stack, inputs=rec3d_in, dtype=rec3d_in.dtype)
         data = tf.expand_dims(tf.concat([forward, backward], 2), 2)
 
         # send output to characters
-        weight = tf.Variable(tf.random_normal([1, 1, rnn_size * 2, len(self.characters) + 1]))
-        self.rec3d_out = tf.squeeze(tf.nn.atrous_conv2d(data, weight, 1, padding='SAME'), axis=[2])
+        weight = tf.Variable(tf.random_normal(
+            [1, 1, rnn_size * 2, len(self.characters) + 1]))
+        self.rec3d_out = tf.squeeze(tf.nn.atrous_conv2d(
+            data, weight, 1, padding='SAME'), axis=[2])
 
     def Sparse(self, texts):
         '''Extract known words into sparse tensors for cost calculations'''
@@ -115,22 +147,40 @@ class Neural_Network:
     def Connectionist_Temporal_Classification(self):
         '''Create the cost and optimiser functions for training, as well as determine the word is when predicting'''
         self.ctc3d_in = tf.transpose(self.rec3d_out, [1, 0, 2])
-        self.texts = tf.SparseTensor(tf.placeholder(tf.int64, shape=[None, 2]), tf.placeholder(tf.int32, [None]), tf.placeholder(tf.int64, [2]))
+        self.texts = tf.SparseTensor(
+            tf.placeholder(
+                tf.int64, shape=[
+                    None, 2]), tf.placeholder(
+                tf.int32, [None]), tf.placeholder(
+                    tf.int64, [2]))
 
         # calculation for cost
         self.seq_len = tf.placeholder(tf.int32, [None])
-        self.cost = tf.reduce_mean(tf.nn.ctc_loss(inputs=self.ctc3d_in, labels=self.texts, sequence_length=self.seq_len, ctc_merge_repeated=True))
-        self.saved_input = tf.placeholder(tf.float32, shape=[Neural_Network.text_len, None, len(self.characters) + 1])
-        self.character_cost = tf.nn.ctc_loss(inputs=self.saved_input, labels=self.texts, sequence_length=self.seq_len, ctc_merge_repeated=True)
+        self.cost = tf.reduce_mean(
+            tf.nn.ctc_loss(
+                inputs=self.ctc3d_in,
+                labels=self.texts,
+                sequence_length=self.seq_len,
+                ctc_merge_repeated=True))
+        self.saved_input = tf.placeholder(
+            tf.float32, shape=[
+                Neural_Network.text_len, None, len(
+                    self.characters) + 1])
+        self.character_cost = tf.nn.ctc_loss(
+            inputs=self.saved_input,
+            labels=self.texts,
+            sequence_length=self.seq_len,
+            ctc_merge_repeated=True)
 
         # determine word
-        self.wordsearch = tf.nn.ctc_greedy_decoder(inputs=self.ctc3d_in, sequence_length=self.seq_len)
+        self.wordsearch = tf.nn.ctc_greedy_decoder(
+            inputs=self.ctc3d_in, sequence_length=self.seq_len)
 
     def word_produce(self, ctc_out, batch_size):
         '''Extract texts from CTC'''
         str_codes = [[] for i in range(batch_size)]
 
-        # retrieve Spare Tensor
+        # retrieve Sparse Tensor
         output = ctc_out[0][0]
 
         # map from batch to words
@@ -156,14 +206,19 @@ class Neural_Network:
             rate = 0.0001
 
         evals = [self.optimiser, self.cost]
-        
-        (_, cost) = self.sess.run(evals, feed_dict={self.batches: batch.images, self.texts: sparse, self.seq_len: [Neural_Network.text_len] * elements, self.rate: rate})
+
+        (_,
+         cost) = self.sess.run(evals,
+                               feed_dict={self.batches: batch.images,
+                                          self.texts: sparse,
+                                          self.seq_len: [Neural_Network.text_len] * elements,
+                                          self.rate: rate})
         self.progress += 1
         return cost
 
     def output(self, rnn_out):
         '''Output the neural network results into CSV files'''
-        output_dir = '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/outputs/'
+        output_dir = filepath.fn_filepath + '/outputs/'
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
 
@@ -185,7 +240,10 @@ class Neural_Network:
         eval_req = [self.wordsearch]
         if rnn_out:
             eval_req += [self.ctc3d_in]
-        evals = self.sess.run(eval_req, {self.batches: batch.images, self.seq_len: [Neural_Network.text_len] * elements})
+        evals = self.sess.run(
+            eval_req, {
+                self.batches: batch.images, self.seq_len: [
+                    Neural_Network.text_len] * elements})
         word = evals[0]
         texts = self.word_produce(word, elements)
 
@@ -197,5 +255,8 @@ class Neural_Network:
     def save(self):
         '''Save the Neural Network to a file'''
         self.current += 1
-        self.saver.save(self.sess, '/Users/naveenkesarwani/Documents/Machine_Learning/SDD Task/save/latest', global_step=self.current)
-
+        self.saver.save(
+            self.sess,
+            filepath.fn_filepath +
+            '/save/latest',
+            global_step=self.current)
